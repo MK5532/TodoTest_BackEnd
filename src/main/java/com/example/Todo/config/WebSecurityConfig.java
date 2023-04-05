04d60555eab47d6adf7e828be66ccbd41865145a
MK5532 <121079522+MK5532@users.noreply.github.com>
2023-04-05 15:21:28 +0900

    프론트 엔드 연동 15. 클라이언트 사이드 렌더링, JWT Token, Spring Security를 이용한 로그인 로직 구현

--- src/main/java/com/example/Todo/config/WebSecurityConfig.java
+++ src/main/java/com/example/Todo/config/WebSecurityConfig.java
@@ -1,19 +1,35 @@
 package com.example.Todo.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.web.filter.CorsFilter;
 
 import com.example.Todo.security.JwtAuthenticationFilter;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
 
 @EnableWebSecurity
 @Slf4j
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	private final ObjectMapper objectMapper;
+	
+	@Autowired
+	public WebSecurityConfig(ObjectMapper objectMapper) {
+		this.objectMapper = objectMapper;
+	}
+	
 	@Autowired
 	private JwtAuthenticationFilter jwtAuthenticationFilter;
 	
@@ -22,6 +38,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		http.cors()
 		.and()
 		.csrf()
+		.disable()
+		.httpBasic()
 		.disable()
 		.sessionManagement()
 		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -33,6 +51,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 		.and()
 		.headers().frameOptions().disable();
 		
+		http.exceptionHandling()
+		.authenticationEntryPoint((request, response, e)->{
+			Map<String,Object> data = new HashMap<String, Object>();
+			data.put("status", HttpServletResponse.SC_FORBIDDEN);
+			data.put("message", e.getMessage());
+			
+			response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+			response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+			
+			objectMapper.writeValue(response.getOutputStream(), data);
+		});
+		
+		
 		http.addFilterAfter(jwtAuthenticationFilter, CorsFilter.class);
+
 	}
 }
